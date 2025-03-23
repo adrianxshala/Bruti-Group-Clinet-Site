07e1dc1ae85854648c73e3ffe5837208fcd1c66b
Adrian Shala <adrianshalasuharek@gmail.com>
2025-03-23 22:46:01 +0100

    dd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
-
 import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
-import AppleSlider from "./components/Sllider";
 import Products from "./components/Services";
 import Footer from "./components/footer";
-import About from "./components/About";
 import AboutSection from "./components/About";
-
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <AppleSlider />
+
       <Products />
       <AboutSection />
       <Footer />
