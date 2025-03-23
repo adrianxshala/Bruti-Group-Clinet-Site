80901f0df66d0dede21c85d7e1406559c398cf01
Adrian Shala <adrianshalasuharek@gmail.com>
2025-03-23 23:14:54 +0100

    ss

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import CountUp from "react-countup";
-
+import { motion } from "framer-motion";
 const AboutSection = () => {
   const [startCounting, setStartCounting] = useState(false);
 
@@ -27,28 +27,53 @@ const AboutSection = () => {
     <section id="about" className="relative py-20 overflow-hidden bg-grid">
       <div className="relative z-10 max-w-7xl mx-auto px-6 lg:px-8">
         <header className="text-center mb-8">
-          <h2 className="text-3xl sm:text-4xl font-bold mb-8 text-center text-transparent bg-clip-text bg-gradient-to-r from-green-950 via-green-900 to-cyan-500 glow">
+          <motion.h2
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.5 }}
+            className="text-3xl sm:text-4xl font-bold mb-8 text-center text-transparent bg-clip-text bg-gradient-to-r from-green-950 via-green-900 to-cyan-500 glow"
+          >
             Rreth Nesh
-          </h2>
-          <p className="text-gray-600 max-w-3xl mx-auto">
-            Ne jemi lider në ndërtimet komerciale, duke sjellë inovacion, cilësi dhe qëndrueshmëri në çdo projekt që realizojmë.
-          </p>
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.7 }}
+            className="text-gray-600 max-w-3xl mx-auto"
+          >
+            Ne jemi lider në ndërtimet komerciale, duke sjellë inovacion, cilësi
+            dhe qëndrueshmëri në çdo projekt që realizojmë.
+          </motion.p>
         </header>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-16 items-center">
           <div className="space-y-10">
             <div className="grid grid-cols-2 gap-6">
               {[
-                { value: 100, label: "Projekte të përfunduara", color: "from-green-950 via-green-900 to-cyan-500" },
-                { value: 20, label: "Partnerë të besuar", color: "blue-400" },
+                {
+                  value: 100,
+                  label: "Projekte të përfunduara",
+                  color: "from-green-950 via-green-900 to-cyan-500",
+                },
+                { value: 20, label: "Partnerë të besuar", color: "blue" },
                 { value: 500, label: "Klientë të kënaqur", color: "green-500" },
                 { value: 25, label: "Vite eksperiencë", color: "gray-300" },
               ].map((item, index) => (
-                <div key={index} className={`relative p-6 rounded-3xl border border-green-950 bg-black shadow-lg glow`}>
+                <motion.div
+                  key={index}
+                  initial={{ opacity: 0, x: 20 }}
+                  whileInView={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 0.6,delay: 0.5 }}
+                  className={`relative p-6 rounded-3xl border  border-green-950 bg-black shadow-lg glow`}
+                >
                   <div className={`text-4xl font-extrabold text-${item.color}`}>
-                    {startCounting ? <CountUp end={item.value} duration={3} /> : 0}
+                    {startCounting ? (
+                      <CountUp end={item.value} duration={3} />
+                    ) : (
+                      0
+                    )}
                   </div>
                   <div className="mt-2 text-sm text-gray-600">{item.label}</div>
-                </div>
+                </motion.div>
               ))}
             </div>
             <div className="hidden md:block">
@@ -56,21 +81,25 @@ const AboutSection = () => {
                 Misioni ynë
               </h2>
               <p className="text-gray-600">
-                Të ofrojmë zgjidhje ndërtimi cilësore dhe të qëndrueshme, duke transformuar idetë në realitet me teknologji të avancuar dhe ekspertizë të lartë.
+                Të ofrojmë zgjidhje ndërtimi cilësore dhe të qëndrueshme, duke
+                transformuar idetë në realitet me teknologji të avancuar dhe
+                ekspertizë të lartë.
               </p>
             </div>
           </div>
-          <div className="relative">
+          <motion.div   initial={{ opacity: 0, x: -20 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.7,delay: 0.7 }} className="relative ">
             <Image
-              src="/assets/presa.jpg"
+              src="/assets/Untitled.png"
               alt="Ndërtimi Komercial – Rreth Nesh"
               title="Ndërtimi Komercial – Rreth Nesh"
               width={600}
               height={400}
               loading="lazy"
-              className="relative rounded-xl shadow-3xl"
+              className="relative rounded-xl shadow-3xl opacity-60 border border-green-600"
             />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -29,20 +29,38 @@ const Sherbimet = () => {
         <div className="grid-overlay"></div>
       </div>
 
-      <div className="container mx-auto px-4 sm:px-6 relative z-10 ">
-        <h2 className="text-3xl sm:text-4xl font-bold mb-12 text-center text-transparent bg-clip-text bg-gradient-to-r from-green-950 via-green-900 to-cyan-500 glow">
+      <div className="container mx-auto px-4 sm:px-6 relative z-10">
+        <motion.h2
+          className="text-3xl sm:text-4xl font-bold mb-8 text-center text-transparent bg-clip-text bg-gradient-to-r from-green-950 via-green-900 to-cyan-500 glow"
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6,delay: 0.5 }}
+        >
           Shërbimet Tona
-        </h2>
-        <p className="text-gray-400 text-center max-w-2xl mx-auto mb-12">
+        </motion.h2>
+
+        <motion.p
+          className="text-gray-400 text-center max-w-2xl mx-auto mb-12"
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.9, delay: 0.7 }}
+        >
           Ne ofrojmë zgjidhje ndërtimi dhe menaxhimi të nivelit të lartë për projekte komerciale dhe industriale, duke garantuar cilësi dhe efikasitet.
-        </p>
+        </motion.p>
+
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8">
           {sherbimet.map((sherbim, index) => (
             <motion.div
               key={index}
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.9, delay: index * 0.1 }}
+              initial={{ opacity: 0, scale: 0.95 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 1,
+                delay: index * 0.9,
+                type: 'spring',
+                stiffness: 300,
+                damping: 20,
+              }}
             >
               {/* Kontejneri i kartës */}
               <div className="relative p-6 sm:p-8 bg-black/10 border border-white/10 rounded-xl flex flex-col items-center text-center h-full backdrop-blur-md overflow-hidden">
